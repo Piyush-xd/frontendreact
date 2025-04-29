9018bfb40afe2b52beae1f9233b3f4634199741f
Piyush-xd <121483457+Piyush-xd@users.noreply.github.com>
2025-04-29 13:22:27 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ import ProductDetail from "./Components/ProductDetail";
 function App() {
   return (
     <>
-    <h1> hellllo</h1>
+    <h1> Vinay Verma</h1>
     <Router>
     <ScrollToTop/>
       <NavBar />
